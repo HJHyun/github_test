64a3793771d2a4710a2d6993fedc1486b29bd76d
HJHyun <heejung.j.hyun@gmail.com>
2016-07-15 19:25:40 +0900

    add HelloWorld project

---
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,13 @@
+// HelloWorld.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl;
+    return 0;
+}
+
